cac062d73b320929fcf5824739cce77f8cdfd844
sakil shaikh <sakilxotwod@gmail.com>
2025-10-27 06:28:29 +0530

    code

---
+++ src/utils.cpp
@@ -0,0 +1,22 @@
+
+#include "utils.hpp"
+#include <sstream>
+
+std::vector<std::string> split(const std::string& s, char delim) {
+    std::vector<std::string> out;
+    std::stringstream ss(s);
+    std::string item;
+    while (std::getline(ss, item, delim)) {
+        out.push_back(item);
+    }
+    return out;
+}
+
+std::string join(const std::vector<std::string>& parts, char delim) {
+    std::string out;
+    for (size_t i=0;i<parts.size();++i) {
+        out += parts[i];
+        if (i+1<parts.size()) out.push_back(delim);
+    }
+    return out;
+}
